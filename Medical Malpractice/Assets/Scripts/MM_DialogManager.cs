36f55588cbfb26a14f1d0c217e124b1063f57126
eva040100 <k00267259@student.tus.ie>
2025-12-21 16:54:34 +0000

    Cleaning up part 2

--- Medical Malpractice/Assets/Scripts/MM_DialogManager.cs
+++ Medical Malpractice/Assets/Scripts/MM_DialogManager.cs
@@ -6,18 +6,10 @@ using UnityEngine.UI;
 
 public class MM_DialogManager : MonoBehaviour
 {
-    public List<TMP_Text> kentLines = new List<TMP_Text>();
-    public int clickCount;
 
     void Start()
     {
-        clickCount = 0;
-        for (int i = 0; i < kentLines.Count; i++)
-        {
-            kentLines[i].enabled = false;
-        }
 
-        kentLines[clickCount].enabled = true;
     }
 
    
@@ -26,10 +18,4 @@ public class MM_DialogManager : MonoBehaviour
         
     }
 
-    public void clickNext()
-    {
-        kentLines[clickCount].enabled = false;
-        clickCount = clickCount + 1;
-        kentLines[clickCount].enabled = true;
-    }
 }
